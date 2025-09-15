a1bc695faeb7f41cc15e3ee3bc7762e6219e1bb6
Timilehin <neowinty8@gmail.com>
2025-09-15 22:33:46 +0100

    Add basic_operations and simple_interest scripts

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+diff_result = number1 - number2
+product_result = number1 * number2
+
+print("The sum of", number1, "and", number2, "is:", sum_result)
+print("The difference of", number1, "and", number2, "is:", diff_result)
+print("The product of", number1, "and", number2, "is:", product_result)
+
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# simple_interest.py
+
+principal = 1000     # initial investment
+rate = 0.05          # 5% annual interest
+time = 3             # 3 years
+
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
+
